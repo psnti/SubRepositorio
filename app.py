4b7cbac122f3c71a4c5b00536cfaf8b8a36e7237
psnti <pablosantitudi64@gmail.com>
2020-04-23 22:13:45 +0200

    Corrección de errores y mejoras visuales.
    Arreglado fallo en css cuando se incorporaban los mapas.
    Cambio iconos de contacto.
    Añado opciones de playa y boton de consulta.

--- app.py
+++ app.py
@@ -13,33 +13,32 @@ Bootstrap(app)
 def paginaPrincipal():
     return render_template('index.html')
 
-@app.route('/mapas')
-def paginaPrediccion():
-    start_coords = (-34.536267, -72.406639)
-    folium_map = folium.Map(location=start_coords, zoom_start=5)
-    print(os.getcwd())
-    print('')
-    folium_map.save('web\\Flask Web\\templates\\mapaChile.html')
-    return render_template('mapas.html')
-
 @app.route('/contacto')
 def paginaContacto():
     return render_template('contacto.html')
 
+@app.route('/mapas')
+def paginaMapas():
+    start_coords = (-34.536267, -72.406639)
+    folium_map = folium.Map(location=start_coords, zoom_start=5)
+    print(os.getcwd())
+    print('')
+    folium_map.save('templates\\mapaChile.html')
+    arregla_mapa()
+    return render_template('mapas.html')
 
 @app.route('/pruebas')
-def index():
+def paginaprueba():
     start_coords = (-34.536267, -72.406639)
     folium_map = folium.Map(location=start_coords, zoom_start=5)
     gjson = os.path.join(app.root_path, 'geojson/prueba.geojson')
     file = open(gjson,encoding='utf-8')
     geo_data = json.load(file)
     file.close()
-    print(geo_data)
+    #print(geo_data)
     #imprimir playas
     w = geo_data['geometry']
-    df = pd.read_excel('web\\Flask Web\\geojson\\playas.xlsx')
-    print(df)
+    df = pd.read_excel('geojson\\playas.xlsx')
     #for x,y in enumerate(w['coordjuinates']):
     for x,y in df.iterrows():
         #print('-----------------------------------',x,y)
@@ -50,8 +49,26 @@ def index():
             popup=str(df.loc[x]['Latitud']).replace(',','.') +''+''+ str(df.loc[x]['Longitud']).replace(',','.')
         ).add_to(folium_map)
         
-    folium_map.save('web\\Flask Web\\templates\\mapaChile.html')
+    folium_map.save('templates\\mapaChile.html')
+    
+    arregla_mapa()
+
     return render_template('mapas.html')
+
+
+def arregla_mapa():
+    f = open('templates\\mapaChile.html','r')
+    filedata = f.read()
+    f.close()
+
+    newdata = filedata.replace('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>','')
+    newdata = newdata.replace('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>','')
+    newdata = newdata.replace('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"/>','')
+    # newdata = newdata.replace('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>','')
+
+    f = open('templates\\mapaChile.html','w')
+    f.write(newdata)
+    f.close()
 
 if __name__ == '__main__':
     app.run(debug = True)
