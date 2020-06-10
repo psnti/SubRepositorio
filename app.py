3496dd1605a105b36cec81f447089c398aa078cd
psnti <52669634+psnti@users.noreply.github.com>
2020-06-10 20:23:12 +0200

    Añadido zoom a playas

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory,request, redirect, url_for,make_response,flash
 from flask_bootstrap import Bootstrap
 import folium
 import os
 import json
 import pandas as pd
+from time import time
+from random import random
 
 
 app = Flask(__name__)
+app.secret_key = 'super clave secreta'
 Bootstrap(app)
+
+df = pd.read_excel('geojson\\playasCoordenadas.xlsx')
+df = df.to_dict()
+folium_map = folium.Map()
 
 
 @app.route('/favicon.ico')
@@ -26,133 +33,60 @@ def paginaContacto():
     return render_template('contacto.html')
 
 
-@app.route('/mapas')
+@app.route('/mapas', methods=['GET','POST'])
 def paginaMapas():
+    if request.method == 'POST':
+        # variables
+        var = request.form['eleccion']
+        fecha = request.form['fecha']
+        if 'Ninguna' not in var:
+            if fecha == '':
+                flash('Introduce una Fecha')
+                return redirect(request.url)
+            # recojo coordenadas de la playa
+            coordenada = df[var]
+            print(coordenada)
+            # agrego marcador al mapa y centro en coordenadas
+            folium_map = folium.Map(location=[coordenada[0],coordenada[1]], zoom_start=15)
+            folium.Marker(
+                location=[coordenada[0],coordenada[1]],
+                popup=var,
+                icon=folium.Icon(color='blue', icon='cloud')
+            ).add_to(folium_map)
+            # guardo mapa
+            folium_map.save('templates\\mapaChile.html')
+            arregla_mapa()
+            #genera_resultados()
+            return render_template('mapas.html', playas = df.keys(), resultados = var, fecha = fecha)
+
+
+    # creo mapa en coordenadas
     start_coords = (-34.536267, -72.406639)
     folium_map = folium.Map(location=start_coords, zoom_start=5)
-    print(os.getcwd())
-    print('')
+    # guardo mapa
+    folium_map.save('templates\\mapaChile.html')
+    # elimino libreria en conflicto
+    arregla_mapa()
+    return render_template('mapas.html',playas = df.keys(),resultados = None)
+
+
+        
+
+def genera_resultados(fecha, coordenadas):
+    pass
+
+
+## zoom a playa especifica
+@app.route('/pruebas3', methods=['GET','POST'])
+def paginaPlayas():
+    
+    start_coords = (-34.536267, -72.406639)
+    folium_map = folium.Map(location=start_coords, zoom_start=5)
     folium_map.save('templates\\mapaChile.html')
     arregla_mapa()
-    return render_template('mapas.html')
+    return render_template('prueba.html', playas = df.keys(),resultados = None)
 
-
-@app.route('/pruebas')
-def paginaprueba():
-    start_coords = (-37.25, -73.58333333333333)
-    folium_map = folium.Map(location=start_coords, zoom_start=10)
-    gjson = os.path.join(app.root_path, 'geojson/prueba.geojson')
-    file = open(gjson, encoding='utf-8')
-    geo_data = json.load(file)
-    file.close()
-    # print(geo_data)
-    # imprimir playas
-    w = geo_data['geometry']
-    df_0083 = pd.read_excel('geojson\\playas0083_1.xlsx')
-    df_025 = pd.read_excel('geojson\\playas025_1.xlsx')
-    df = pd.read_excel('geojson\\playasSinRedondeoCoord.xlsx')
-
-    # for x,y in enumerate(w['coordjuinates']):
-    for x, y in df_0083.iterrows():
-        # print('-----------------------------------',x,y)
-        folium.Marker(
-            # location=[y[0],y[1]],
-            # popup=w["nombre"][x]
-            location=[str(df_0083.loc[x]['Latitud']).replace(',', '.'),
-                      str(df_0083.loc[x]['Longitud']).replace(',', '.')],
-            popup="0,083" + str(df_0083.loc[x]['Latitud']).replace(',', '.') + \
-            ''+'' + str(df_0083.loc[x]['Longitud']).replace(',', '.'),
-            icon=folium.Icon(color='red', icon='cloud')
-        ).add_to(folium_map)
-    for x, y in df_025.iterrows():
-        # print('-----------------------------------',x,y)
-        folium.Marker(
-            # location=[y[0],y[1]],
-            # popup=w["nombre"][x]
-            location=[str(df_025.loc[x]['Latitud']).replace(',', '.'),
-                      str(df_025.loc[x]['Longitud']).replace(',', '.')],
-            popup="0,25" + str(df_025.loc[x]['Latitud']).replace(',', '.') + \
-            ''+'' + str(df_025.loc[x]['Longitud']).replace(',', '.'),
-            icon=folium.Icon(color='blue')
-        ).add_to(folium_map)
-
-    for x, y in df.iterrows():
-        # print('-----------------------------------',x,y)
-        folium.Marker(
-            # location=[y[0],y[1]],
-            # popup=w["nombre"][x]
-            location=[str(df.loc[x]['Latitud']).replace(',', '.'),
-                      str(df.loc[x]['Longitud']).replace(',', '.')],
-            popup="Original" + str(df.loc[x]['Latitud']).replace(',', '.') + \
-            ''+'' + str(df.loc[x]['Longitud']).replace(',', '.'),
-            icon=folium.Icon(color='green')
-        ).add_to(folium_map)
-    folium_map.save('templates\\mapaChile.html')
-
-    arregla_mapa()
-
-    return render_template('mapas.html')
-
-
-@app.route('/pruebas2')
-def paginaprueba2():
-    start_coords = (-37.25, -73.58333333333333)
-    folium_map = folium.Map(location=start_coords, zoom_start=10)
-    gjson = os.path.join(app.root_path, 'geojson/prueba.geojson')
-    file = open(gjson, encoding='utf-8')
-    geo_data = json.load(file)
-    file.close()
-    # print(geo_data)
-    # imprimir playas
-    w = geo_data['geometry']
-    df_0083 = pd.read_excel('geojson\\playas0083_2.xlsx')
-    df_025 = pd.read_excel('geojson\\playas025_2.xlsx')
-    df = pd.read_excel('geojson\\playasSinRedondeoCoord.xlsx')
-
-    # for x,y in enumerate(w['coordjuinates']):
-
-    for x, y in df_025.iterrows():
-        # print('-----------------------------------',x,y)
-        folium.Marker(
-            # location=[y[0],y[1]],
-            # popup=w["nombre"][x]
-            location=[str(df_025.loc[x]['Latitud']-0.001).replace(',', '.'),
-                      str(df_025.loc[x]['Longitud']-0.001).replace(',', '.')],
-            popup="0,25" + str(df_025.loc[x]['Latitud']).replace(',', '.') + \
-            ''+'' + str(df_025.loc[x]['Longitud']).replace(',', '.'),
-            icon=folium.Icon(color='blue')
-        ).add_to(folium_map)
-
-    for x, y in df_0083.iterrows():
-        # print('-----------------------------------',x,y)
-        folium.Marker(
-            # location=[y[0],y[1]],
-            # popup=w["nombre"][x]
-            location=[str(df_0083.loc[x]['Latitud']).replace(',', '.'),
-                      str(df_0083.loc[x]['Longitud']).replace(',', '.')],
-            popup="0,083" + str(df_0083.loc[x]['Latitud']).replace(',', '.') + \
-            ''+'' + str(df_0083.loc[x]['Longitud']).replace(',', '.'),
-            icon=folium.Icon(color='red', icon='cloud')
-        ).add_to(folium_map)
-
-    for x, y in df.iterrows():
-        # print('-----------------------------------',x,y)
-        folium.Marker(
-            # location=[y[0],y[1]],
-            # popup=w["nombre"][x]
-            location=[str(df.loc[x]['Latitud']).replace(',', '.'),
-                      str(df.loc[x]['Longitud']).replace(',', '.')],
-            popup="Original" + str(df.loc[x]['Latitud']).replace(',', '.') + \
-            ''+'' + str(df.loc[x]['Longitud']).replace(',', '.'),
-            icon=folium.Icon(color='green')
-        ).add_to(folium_map)
-    folium_map.save('templates\\mapaChile.html')
-
-    arregla_mapa()
-
-    return render_template('mapas.html')
-
-
+# elimina javascrip y css en conflicto
 def arregla_mapa():
     f = open('templates\\mapaChile.html', 'r')
     filedata = f.read()
@@ -172,4 +106,148 @@ def arregla_mapa():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    app.run(debug=True, port=1000)
+
+
+# @app.route('/pruebas')
+# def paginaprueba():
+#     start_coords = (-37.25, -73.58333333333333)
+#     folium_map = folium.Map(location=start_coords, zoom_start=10)
+#     gjson = os.path.join(app.root_path, 'geojson/prueba.geojson')
+#     file = open(gjson, encoding='utf-8')
+#     geo_data = json.load(file)
+#     file.close()
+#     # print(geo_data)
+#     # imprimir playas
+#     w = geo_data['geometry']
+#     df_0083 = pd.read_excel('geojson\\playas0083_1.xlsx')
+#     df_025 = pd.read_excel('geojson\\playas025_1.xlsx')
+#     df = pd.read_excel('geojson\\playasSinRedondeoCoord.xlsx')
+
+#     # for x,y in enumerate(w['coordjuinates']):
+#     for x, y in df_0083.iterrows():
+#         # print('-----------------------------------',x,y)
+#         folium.Marker(
+#             # location=[y[0],y[1]],
+#             # popup=w["nombre"][x]
+#             location=[str(df_0083.loc[x]['Latitud']).replace(',', '.'),
+#                       str(df_0083.loc[x]['Longitud']).replace(',', '.')],
+#             popup="0,083" + str(df_0083.loc[x]['Latitud']).replace(',', '.') + \
+#             ''+'' + str(df_0083.loc[x]['Longitud']).replace(',', '.'),
+#             icon=folium.Icon(color='red', icon='cloud')
+#         ).add_to(folium_map)
+#     for x, y in df_025.iterrows():
+#         # print('-----------------------------------',x,y)
+#         folium.Marker(
+#             # location=[y[0],y[1]],
+#             # popup=w["nombre"][x]
+#             location=[str(df_025.loc[x]['Latitud']).replace(',', '.'),
+#                       str(df_025.loc[x]['Longitud']).replace(',', '.')],
+#             popup="0,25" + str(df_025.loc[x]['Latitud']).replace(',', '.') + \
+#             ''+'' + str(df_025.loc[x]['Longitud']).replace(',', '.'),
+#             icon=folium.Icon(color='blue')
+#         ).add_to(folium_map)
+
+#     for x, y in df.iterrows():
+#         # print('-----------------------------------',x,y)
+#         folium.Marker(
+#             # location=[y[0],y[1]],
+#             # popup=w["nombre"][x]
+#             location=[str(df.loc[x]['Latitud']).replace(',', '.'),
+#                       str(df.loc[x]['Longitud']).replace(',', '.')],
+#             popup="Original" + str(df.loc[x]['Latitud']).replace(',', '.') + \
+#             ''+'' + str(df.loc[x]['Longitud']).replace(',', '.'),
+#             icon=folium.Icon(color='green')
+#         ).add_to(folium_map)
+#     folium_map.save('templates\\mapaChile.html')
+
+#     arregla_mapa()
+
+#     return render_template('mapas.html')
+
+
+
+# @app.route('/pruebas2')
+# def paginaprueba2():
+#     start_coords = (-37.25, -73.58333333333333)
+#     folium_map = folium.Map(location=start_coords, zoom_start=10)
+#     gjson = os.path.join(app.root_path, 'geojson/prueba.geojson')
+#     file = open(gjson, encoding='utf-8')
+#     geo_data = json.load(file)
+#     file.close()
+#     # print(geo_data)
+#     # imprimir playas
+#     w = geo_data['geometry']
+#     df_0083 = pd.read_excel('geojson\\playas0083_2.xlsx')
+#     df_025 = pd.read_excel('geojson\\playas025_2.xlsx')
+#     df = pd.read_excel('geojson\\playasSinRedondeoCoord.xlsx')
+
+#     # for x,y in enumerate(w['coordjuinates']):
+
+#     for x, y in df_025.iterrows():
+#         # print('-----------------------------------',x,y)
+#         folium.Marker(
+#             # location=[y[0],y[1]],
+#             # popup=w["nombre"][x]
+#             location=[str(df_025.loc[x]['Latitud']-0.001).replace(',', '.'),
+#                       str(df_025.loc[x]['Longitud']-0.001).replace(',', '.')],
+#             popup="0,25" + str(df_025.loc[x]['Latitud']).replace(',', '.') + \
+#             ''+'' + str(df_025.loc[x]['Longitud']).replace(',', '.'),
+#             icon=folium.Icon(color='blue')
+#         ).add_to(folium_map)
+
+#     for x, y in df_0083.iterrows():
+#         # print('-----------------------------------',x,y)
+#         folium.Marker(
+#             # location=[y[0],y[1]],
+#             # popup=w["nombre"][x]
+#             location=[str(df_0083.loc[x]['Latitud']).replace(',', '.'),
+#                       str(df_0083.loc[x]['Longitud']).replace(',', '.')],
+#             popup="0,083" + str(df_0083.loc[x]['Latitud']).replace(',', '.') + \
+#             ''+'' + str(df_0083.loc[x]['Longitud']).replace(',', '.'),
+#             icon=folium.Icon(color='red', icon='cloud')
+#         ).add_to(folium_map)
+
+#     for x, y in df.iterrows():
+#         # print('-----------------------------------',x,y)
+#         folium.Marker(
+#             # location=[y[0],y[1]],
+#             # popup=w["nombre"][x]
+#             location=[str(df.loc[x]['Latitud']).replace(',', '.'),
+#                       str(df.loc[x]['Longitud']).replace(',', '.')],
+#             popup="Original" + str(df.loc[x]['Latitud']).replace(',', '.') + \
+#             ''+'' + str(df.loc[x]['Longitud']).replace(',', '.'),
+#             icon=folium.Icon(color='green')
+#         ).add_to(folium_map)
+#     folium_map.save('templates\\mapaChile.html')
+
+#     arregla_mapa()
+
+#     return render_template('mapas.html')
+
+
+
+# @app.route('/grafico')
+# def grafico():
+#     return render_template('grafico.html', diccionario = dic_graf, resultados = None)
+
+# @app.route('/data', methods=["GET", "POST"])
+# def data():
+    
+#     data = dic_graf[str(dic_graf['contador'])]
+#     print(data)
+#     response = make_response(json.dumps(data))
+#     response.content_type = 'application/json'
+#     print(response)
+#     dic_graf['contador'] = dic_graf['contador'] + 1
+#     return response
+
+# @app.route('/data', methods=["GET", "POST"])
+# def data():
+#     data = [time() * 1000, random() * 100]
+#     print(data)
+#     response = make_response(json.dumps(data))
+#     response.content_type = 'application/json'
+#     print(response)
+#     return response
