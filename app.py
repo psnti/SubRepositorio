8b5cfd9d5448d90d760730ddbfb865ded9def041
psnti <52669634+psnti@users.noreply.github.com>
2020-06-18 20:04:59 +0200

    Introducido gr'afico de historico de avistamientos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory,request, redirect, url_for,make_response,flash
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for, make_response, flash
 from flask_bootstrap import Bootstrap
 import folium
 import os
@@ -9,6 +9,7 @@ from random import random
 from joblib import load
 import sklearn
 from folium.plugins import MarkerCluster
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -32,11 +33,11 @@ def paginaContacto():
     return render_template('contacto.html')
 
 
-@app.route('/mapas', methods=['GET','POST'])
+@app.route('/mapas', methods=['GET', 'POST'])
 def paginaMapas():
     # df = pd.read_excel('geojson/playasCoordenadas.xlsx')
-    df = pd.read_pickle('geojson/listado_playas.pkl')
-    # df = df.to_dict()   
+    df = pd.read_pickle('documentos/listado_playas.pkl')
+    # df = df.to_dict()
     if request.method == 'POST':
         # variables
         var = request.form['eleccion']
@@ -48,72 +49,83 @@ def paginaMapas():
                 return redirect(request.url)
 
             # recojo coordenadas de la playa
-            coordenada = [-df['Lat.dec'][var],-df['Long.dec'][var]]
+            coordenada = [-df['Lat.dec'][var], -df['Long.dec'][var]]
             print(coordenada)
             # agrego marcador al mapa y centro en coordenadas
             folium_map_especifico = folium.Map()
-            folium_map_especifico = folium.Map(location=[coordenada[0],coordenada[1]], zoom_start=15)
+            folium_map_especifico = folium.Map(
+                location=[coordenada[0], coordenada[1]], zoom_start=15)
             folium.Marker(
-                location=[coordenada[0],coordenada[1]],
+                location=[coordenada[0], coordenada[1]],
                 popup=var,
                 icon=folium.Icon(color='blue', icon='cloud')
             ).add_to(folium_map_especifico)
             mapa = folium_map_especifico._repr_html_()
-
-            #resulados 
+            historico = dame_historico(var)
+            #resulados
             resultados = genera_resultados(fecha, coordenada)
-            return render_template('mapas.html', playas = df.index.values, nombre = var, resultados = resultados, fecha = fecha, mapa = mapa)
-
+            return render_template('mapas.html', playas=df.index.values, nombre=var, resultados=resultados, historico=historico, fecha=fecha, mapa=mapa)
 
     # creo mapa en coordenadas
     print('mapa original')
     folium_map = folium.Map()
     start_coords = (-34.536267, -72.406639)
     folium_map = folium.Map(location=start_coords, zoom_start=5)
-    folium_map = anade_playas(folium_map,df)
+    folium_map = anade_playas(folium_map, df)
     # elimino libreria en conflicto
     mapa = arregla_mapa(folium_map._repr_html_())
-    return render_template('mapas.html',playas = df.index.values,resultados = None, mapa = mapa)
+    return render_template('mapas.html', playas=df.index.values, resultados=None, historico = None, mapa=mapa)
 
-def anade_playas(fol,playas):
+
+def dame_historico(nombre_playa):
+    excel = pd.read_excel('documentos/Datos_Physalia_20171010.xls')
+    seleccion = excel.loc[excel['Lugar'] == nombre_playa]
+    seleccion = seleccion.groupby(['Date']).sum().reset_index()
+    
+    lista = []
+    for i in seleccion.iterrows():
+        lista.append({'y':(i[1]['Date']).strftime("%Y-%m-%d"),'v':i[1]['Abundancia']})
+
+    return lista
+    
+
+
+def anade_playas(fol, playas):
     icon_url = 'static/pics/jellyfish.png'
     cluster = MarkerCluster().add_to(fol)
     for i in playas.iterrows():
         # print('\n{}\n'.format(type(i[1])))
-        folium.Marker([-i[1][0],-i[1][1]],
-                        popup=i[0],
-                        icon=folium.Icon(color='red', icon='info-sign'),
-                        ).add_to(cluster)
+        folium.Marker([-i[1][0], -i[1][1]],
+                      popup=i[0],
+                      icon=folium.Icon(color='red', icon='info-sign'),
+                      ).add_to(cluster)
     return fol
+
 
 def borra_mapa():
     try:
         os.remove('templates/mapaChile.html')
         print(os.listdir('templates'))
-    except :
+    except:
         print('Aun no existe')
 
-def genera_resultados(fecha,coodenadas):
+
+def genera_resultados(fecha, coodenadas):
     # se cogerian los datos de copernicus
     # generar dataframe
     # meterlo al modelo
-    modelo =    load('static/modelo.joblib')
+    modelo = load('static/modelo.joblib')
     # print(modelo)
 
-    lista = [{'y': '2020-1-1','v': 1},
-            {'y': '2020-2-1','v': 10},
-            {'y': '2020-1-5','v': 5},
-            {'y': '2020-3-5','v': 2}]
-    return lista 
+    lista = [{'y': '2020-1-1', 'v': 1},
+             {'y': '2020-2-1', 'v': 10},
+             {'y': '2020-1-5', 'v': 5},
+             {'y': '2020-3-5', 'v': 2}]
+    return lista
 
 
 # elimina javascrip y css en conflicto
 def arregla_mapa(mapa):
-    
-    print(mapa)
-    print(type(mapa))
-    # filedata = f.read()
-    # f.close()
 
     newdata = mapa.replace(
         '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>', '')
@@ -121,37 +133,10 @@ def arregla_mapa(mapa):
         '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>', '')
     newdata = newdata.replace(
         '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"/>', '')
-    # newdata = newdata.replace('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>','')
 
-    # f = open('./templates/mapaChile.html', 'w')
-    # f.write(newdata)
-    # f.close()
     return newdata
-
-# elimina javascrip y css en conflicto
-def arregla_mapa_(mapa):
-    f = open('./templates/mapaChile.html', 'r')
-    print(f)
-    filedata = f.read()
-    f.close()
-
-    newdata = filedata.replace(
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>', '')
-    newdata = newdata.replace(
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>', '')
-    newdata = newdata.replace(
-        '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"/>', '')
-    # newdata = newdata.replace('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>','')
-
-    f = open('./templates/mapaChile.html', 'w')
-    f.write(newdata)
-    f.close()
 
 
 if __name__ == '__main__':
-    
+
     app.run(debug=True, port=1000)
-
-
-
-
