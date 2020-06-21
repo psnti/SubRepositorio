453cb825095854018bab0d6ca6b0284bf1bc9e5d
psnti <52669634+psnti@users.noreply.github.com>
2020-06-21 19:05:13 +0200

    Nuevo test selenium

--- test/test.py
+++ test/test.py
@@ -16,7 +16,7 @@ def inicializar():
     Devuelve el objeto webdriver
     """
     driver_path = 'test/chromedriver/chromedriverv83.exe'
-    driver_path = 'chromedriver/chromedriverv83.exe'
+    # driver_path = 'chromedriver/chromedriverv83.exe'
 
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
@@ -26,6 +26,8 @@ def inicializar():
 
     driver = reinicia(driver,wait)
 
+    time.sleep(2)
+
     return driver,wait
 
 def reinicia(driver,wait):
@@ -34,7 +36,7 @@ def reinicia(driver,wait):
     """
     driver.get("https://jellyfish-forecast.herokuapp.com/")
     assert "Jellyfish" in driver.title
-    boton_mapa = driver.find_elements_by_name('Mapas')[0]
+    boton_mapa = driver.find_element_by_name('Mapas')
     boton_mapa.click()
     return driver
 
@@ -47,28 +49,48 @@ def comprueba_error_fecha_no_introducida(driver,wait):
 
     Parametros:
     driver -- objeto webdriver
-    """
-    select = Select(driver.find_element_by_name('eleccion'))
-    select.select_by_visible_text('Bahía.Coliumo')
-
-    boton = driver.find_element_by_name('boton_consultar')
-    boton.click()
-    print('paso')
-
+    """    
+    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div/div/div/form/select/option[5]').click()
+    time.sleep(2)
+    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div/div/div/form/div[2]/button').click()
+    time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.NAME, 'alerta')))
+
+def comprueba_graficos(driver,wait):
+    # wait.until(EC.element_to_be_clickable((By.ID, 'datepicker')))
+    time.sleep(1)
+    driver.find_element_by_id('datepicker').send_keys('06/12/2020')
+    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div/div/div/form/select/option[5]').click()
+    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div/div/div/form/div[2]/button').click()
+    try:
+        driver.find_element_by_id('data-container')
+    except Exception as identifier:
+        raise Exception('Graficos no mostrados')
 
 def main():
     driver,wait = inicializar() # se abre la pagina de mapas
     try:
-        print('Prueba 1: Se selecciona playa sin una fecha')
-        comprueba_error_fecha_no_introducida(driver,wait)
-        print("Exito")
-    except:
-        print("Fracaso")
-        pass
+        try:
+            print('Prueba 1: Se selecciona playa sin una fecha')
+            comprueba_error_fecha_no_introducida(driver,wait)
+            print("OK, slata mensaje de error")
+        except Exception as e:
+            print("MAL: no salta mensaje de error")
+        try:
+            reinicia(driver,wait)
+            print('Prueba 1: Se selecciona playa sin una fecha')
+            comprueba_graficos(driver,wait)
+            print("OK, aparecen los gráficos")
+        except Exception as e:
+            print("MAL: no aparecen los gráficos")
+    except Exception as identifier:
+        print(type(identifier))
+        print(identifier)
     finally:
-        driver.implicitly_wait(3)
+        time.sleep(10)
         driver.close()
+    
+    
 
 
 if __name__ == '__main__':
