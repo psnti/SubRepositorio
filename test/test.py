0ee9cf05b377e5476c82ed50f37f7887b441a1c3
psnti <52669634+psnti@users.noreply.github.com>
2020-06-21 18:18:49 +0200

    Cambios en nombres para selenium

--- test/test.py
+++ test/test.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select,WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import os
+import time
+
 
 
 def inicializar():
@@ -12,24 +16,30 @@ def inicializar():
     Devuelve el objeto webdriver
     """
     driver_path = 'test/chromedriver/chromedriverv83.exe'
+    driver_path = 'chromedriver/chromedriverv83.exe'
+
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_path)
+    wait = WebDriverWait(driver, 5) # define el tiempo maximo de espera
 
-    driver = reinicia(driver)
-    return driver
+    driver = reinicia(driver,wait)
 
-def reinicia(driver):
+    return driver,wait
+
+def reinicia(driver,wait):
     """"
     Vuelve a la pagina de los mapas 
     """
     driver.get("https://jellyfish-forecast.herokuapp.com/")
     assert "Jellyfish" in driver.title
+    boton_mapa = driver.find_elements_by_name('Mapas')[0]
+    boton_mapa.click()
     return driver
 
 #### TEST ####
-def comprueba_error_fecha_no_introducida(driver):
+def comprueba_error_fecha_no_introducida(driver,wait):
     """
     Comprueba que si se introduce una playa sin fecha lanza mensaje de error
 
@@ -43,13 +53,22 @@ def comprueba_error_fecha_no_introducida(driver):
 
     boton = driver.find_element_by_name('boton_consultar')
     boton.click()
+    print('paso')
+
+    wait.until(EC.element_to_be_clickable((By.NAME, 'alerta')))
 
 def main():
-    driver = inicializar() # se abre la pagina
+    driver,wait = inicializar() # se abre la pagina de mapas
     try:
-        comprueba_error_fecha_no_introducida(driver)
-    except expression as identifier:
+        print('Prueba 1: Se selecciona playa sin una fecha')
+        comprueba_error_fecha_no_introducida(driver,wait)
+        print("Exito")
+    except:
+        print("Fracaso")
         pass
+    finally:
+        driver.implicitly_wait(3)
+        driver.close()
 
 
 if __name__ == '__main__':
